9f2b9375d939000b984cfd404ed287f0e17fe610
Ankush <155977952+ankushthedeveloper@users.noreply.github.com>
2025-04-14 09:21:30 +0530

    updated the file Location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Doctors from './pages/Doctors'
 import About from './pages/About'
